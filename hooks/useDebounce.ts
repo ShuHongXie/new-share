89b047ecc91d3ba9e0a762d6c66a76cfed9b2c66
shuhongxie <384180258@qq.com>
2022-04-08 22:04:40 +0800

    feat: 增加防抖缓存

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,9 +1,12 @@
+import { useRef } from "react";
+
 type DebounceProps = {
   fn: any;
   time?: number;
 };
 
 export default function useDebounce({ fn, time = 1000 }: DebounceProps) {
+  const r = useRef();
   let timer: any;
   return function () {
     // @ts-ignore
